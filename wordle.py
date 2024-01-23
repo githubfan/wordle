0162932b26ea4663bdd2a5c0d628f0ca8d9943b3
Josh K <47252616+githubfan@users.noreply.github.com>
2024-01-23 09:12:12 +0000

    moving commented out code block & whitespace

--- wordle.py
+++ wordle.py
@@ -26,18 +26,7 @@ def validation():
             if userinput == word:
                 print("You've guessed the correct word!")
                 found = True
-#            # checking individual letters
-#            if userinput[0] == word[0]:
-#                print('your first letter is correct')
-#                print(word[0])
-#            elif userinput[0] in word:
-#                print('Letter is in word')
-#                
-#                # make word[0] coloureed
-#                # append word[0] to a display string
-#                # repeat for each leter
-#                # make siome coloured text here
-#       
+
 chooseword()
 # take in input
 found = False
